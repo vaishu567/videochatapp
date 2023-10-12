d7cb3eafb6b7201221e9562389f7c12f6d24ff4f
vaishu567 <rslv3vaishu567@gmail.com>
2023-10-12 16:23:21 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from "./logo.svg";
+import Lobby from "./screens/Lobby";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit this for me <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Lobby />} />
+      </Routes>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const { Server } = require("socket.io");
 
 const io = new Server(8000);
-io.on("connection", (socket) => {});
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+});
