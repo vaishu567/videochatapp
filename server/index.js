ea050b3ebb30d724094d6c4fd1027a07451621a5
vaishu567 <rslv3vaishu567@gmail.com>
2023-10-12 19:20:15 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const { Server } = require("socket.io");
 
-const io = new Server(8000);
+const io = new Server(8000, {
+  cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
 io.on("connection", (socket) => {
   console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(socket.id).emit("room:join", data);
+  });
 });
