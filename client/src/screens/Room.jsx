fa8b85722b71b5db6d5afbc76b7041bbe50ffac8
vaishu567 <rslv3vaishu567@gmail.com>
2023-10-13 18:32:14 +0530

    final commit

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -38,22 +38,38 @@ const Room = () => {
     },
     [socket]
   );
+  ///////////////////////////////////////////////////////
+  const sendStreams = useCallback(() => {
+    for (const track of myStream.getTracks()) {
+      peer.peer.addTrack(track, myStream);
+    }
+  }, [myStream]);
   ////////////////////////////////////////////////////////////////
   const handleCallAccepted = useCallback(
     ({ from, ans }) => {
       peer.setLocalDescription(ans);
-      console.log("Call Accepted!");
-      for (const track of myStream.getTracks()) {
-        peer.peer.addTrack(track, myStream);
-      }
+      console.log("Call Accepted!", from);
+      sendStreams();
     },
-    [myStream]
+    [sendStreams]
+  );
+  ///////////////////////////////////////////////////////////////
+  const handleNegoNeedIncoming = useCallback(
+    async ({ from, offer }) => {
+      const ans = await peer.getAnswer(offer);
+      socket.emit("peer:nego:done", { to: from, ans });
+    },
+    [socket]
   );
   ////////////////////////////////////////////////////////////////////
   const handleNegoNeeded = useCallback(async () => {
     const offer = await peer.getOffer();
     socket.emit("peer:nego:needed", { offer, to: remoteSocketId });
   }, [socket, remoteSocketId]);
+
+  const handleNegoNeedFinal = useCallback(async ({ ans }) => {
+    await peer.setLocalDescription(ans);
+  }, []);
 
   useEffect(() => {
     peer.peer.addEventListener("negotiationneeded", handleNegoNeeded);
@@ -65,7 +81,8 @@ const Room = () => {
   useEffect(() => {
     peer.peer.addEventListener("track", async (ev) => {
       const remotexStream = ev.streams;
-      setRemoteStream(remotexStream);
+      console.log("Got tracks!");
+      setRemoteStream(remotexStream[0]);
     });
   }, []);
   //////////////////////////////////////////////////////////////////////////////////
@@ -73,12 +90,24 @@ const Room = () => {
     socket.on("user:joined", handleUserJoined);
     socket.on("incoming:call", handleIncomingCall);
     socket.on("call:accepted", handleCallAccepted);
+    socket.on("peer:nego:needed", handleNegoNeedIncoming);
+    socket.on("peer:nego:final", handleNegoNeedFinal);
+
     return () => {
       socket.off("user:joined", handleUserJoined);
       socket.off("incoming:call", handleIncomingCall);
       socket.off("call:accepted", handleCallAccepted);
+      socket.off("peer:nego:needed", handleNegoNeedIncoming);
+      socket.off("peer:nego:final", handleNegoNeedFinal);
     };
-  }, [socket, handleUserJoined, handleIncomingCall, handleCallAccepted]);
+  }, [
+    socket,
+    handleUserJoined,
+    handleIncomingCall,
+    handleCallAccepted,
+    handleNegoNeedIncoming,
+    handleNegoNeedFinal,
+  ]);
   ////////////////////////////////////////////////////////////////////
   return (
     <>
@@ -86,6 +115,7 @@ const Room = () => {
       <h4>
         {remoteSocketId ? "Some one is trying to Connect" : "No one in room"}
       </h4>
+      {myStream && <button onClick={sendStreams}>Send Stream</button>}
       {remoteSocketId && <button onClick={handleCallUser}>Call</button>}
       {myStream && (
         <>
