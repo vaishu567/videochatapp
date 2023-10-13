62424665ff501286d472d46be367434fc5e6b24c
vaishu567 <rslv3vaishu567@gmail.com>
2023-10-13 17:18:56 +0530

    eight commit

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -7,6 +7,7 @@ const Room = () => {
   const socket = useSocket();
   const [remoteSocketId, setRemoteSocketId] = useState(null);
   const [myStream, setMyStream] = useState(null);
+  const [remoteStream, setRemoteStream] = useState(null);
   //////////////////////////////////////////////////////////////////
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`Email ${email} joined room`);
@@ -38,12 +39,36 @@ const Room = () => {
     [socket]
   );
   ////////////////////////////////////////////////////////////////
-  const handleCallAccepted = useCallback(({ from, ans }) => {
-    peer.setLocalDescription(ans);
-    console.log("Call Accepted!");
-  }, []);
-  ////////////////////////////////////////////////////////////////
+  const handleCallAccepted = useCallback(
+    ({ from, ans }) => {
+      peer.setLocalDescription(ans);
+      console.log("Call Accepted!");
+      for (const track of myStream.getTracks()) {
+        peer.peer.addTrack(track, myStream);
+      }
+    },
+    [myStream]
+  );
+  ////////////////////////////////////////////////////////////////////
+  const handleNegoNeeded = useCallback(async () => {
+    const offer = await peer.getOffer();
+    socket.emit("peer:nego:needed", { offer, to: remoteSocketId });
+  }, [socket, remoteSocketId]);
 
+  useEffect(() => {
+    peer.peer.addEventListener("negotiationneeded", handleNegoNeeded);
+    return () => {
+      peer.peer.removeEventListener("negotiationneeded", handleNegoNeeded);
+    };
+  }, [handleNegoNeeded]);
+  ////////////////////////////////////////////////////////////////
+  useEffect(() => {
+    peer.peer.addEventListener("track", async (ev) => {
+      const remotexStream = ev.streams;
+      setRemoteStream(remotexStream);
+    });
+  }, []);
+  //////////////////////////////////////////////////////////////////////////////////
   useEffect(() => {
     socket.on("user:joined", handleUserJoined);
     socket.on("incoming:call", handleIncomingCall);
@@ -65,6 +90,18 @@ const Room = () => {
       {myStream && (
         <>
           <h2>My Stream</h2>
+          <ReactPlayer
+            playing
+            muted
+            height={"400px"}
+            width={"400px"}
+            url={myStream}
+          />
+        </>
+      )}
+      {remoteStream && (
+        <>
+          <h2>Remote Stream</h2>
           <ReactPlayer
             playing
             muted
